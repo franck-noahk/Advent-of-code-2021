4f439c5161e123653a9279a773ce705517fadaa1
Noah F <franck.noahk@gmail.com>
2021-12-16 20:36:16 -0600

    D03: Gold star able to get the most and least important bits.

--- Day 03/Program.cs
+++ Day 03/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Day_03
 {
@@ -10,14 +11,69 @@ namespace Day_03
             HandelInputs inputReader = new HandelInputs();
 
             inputReader.ReadFiles();
-            List<String> data = inputReader.TestData;
+            List<String> data = inputReader.InputData;
 
-            foreach (string number in data)
+            string gama = getSignficantBit(data);
+            string epsilon = getLeastSignificantBit(data);
+
+            Console.WriteLine($"Gama: {gama}\tEpsilon: {epsilon}");
+        }
+
+        private static string getSignficantBit(List<string> data)
+        {
+            string toReturn = "";
+            int isOne = 0;
+            char[] charArr;
+            char[] sample = data.First<string>().ToCharArray();
+            for (int i = 0; i < sample.Length; i++)
             {
-                Console.WriteLine (number);
+                foreach (string set in data)
+                {
+                    charArr = set.ToCharArray();
+                    if (charArr[i] == '1')
+                    {
+                        isOne++;
+                    }
+                    else
+                    {
+                        isOne--;
+                    }
+                }
+                if (isOne > 0)
+                {
+                    toReturn += "1";
+                }
+                else
+                {
+                    toReturn += "0";
+                }
+                isOne = 0;
             }
 
-            // Console.WriteLine(inputReader.SplitData()[0]);
+            return toReturn;
+        }
+
+        private static string getLeastSignificantBit(List<string> data)
+        {
+            string toReturn = "";
+
+            string toInvert = getSignficantBit(data);
+
+            char[] charArr = toInvert.ToCharArray();
+
+            for (int i = 0; i < charArr.Length; i++)
+            {
+                if (charArr[i] == '0')
+                {
+                    toReturn += "1";
+                }
+                else
+                {
+                    toReturn += "0";
+                }
+            }
+
+            return toReturn;
         }
     }
 }
